51b6e9a6c8ea4663920cc90e3a161879e22e8e0d
Daniel Pozas <dpozasruiz@gmail.com>
2026-04-16 20:32:00 +0200

    Backtracking terminado

--- P2/bt.cpp
+++ P2/bt.cpp
@@ -6,23 +6,29 @@ using Solucion=vector<int>;
 
 vector<vector<int>> B;
 vector<int> C;
+int T;
 int nw, nt;
+int libres;
 
-void generar(int nivel, Solucion& s, int ben, vector<int>& asigT, vector<int>& asigW){
+// NIVEL es el número de trabajo, s[NIVEL] el trabajador asignado a ese trabajo
 
-    s[nivel]+=1;
+void generar(int &nivel, Solucion& s, int &ben, vector<int>& asigW){
 
-    if(s[nivel]==0){
-        ben+= B[s[nivel]][nivel];
-    }
-    else{
-        ben+= B[s[nivel]][nivel];
-        ben-= B[s[nivel] - 1][nivel];
-    }
+        s[nivel]+=1;
 
-    asigT[nivel]+=1;
-    asigW[s[nivel]]+=1;
+        if(s[nivel]==0){
+            ben+= B[s[nivel]][nivel];
+            asigW[s[nivel]]+=1;
+            libres--;
+        }
+        else{
+            ben+= B[s[nivel]][nivel];
+            asigW[s[nivel]]+=1;
+            ben-= B[s[nivel] - 1][nivel];
+            asigW[s[nivel] - 1]-=1;
+        }
 
+        
 }
 
 bool contiene(vector<int>& v, int valor){
@@ -37,23 +43,41 @@ bool contiene(vector<int>& v, int valor){
     return false;
 }
 
-void retroceder(int nivel, Solucion& s, int ben, vector<int>& asigW, vector<int>& asigT){
+void retroceder(int &nivel, Solucion& s, int &ben, vector<int>& asigW){
     ben-=B[s[nivel]][nivel];
-    s[nivel]=-1;
     asigW[s[nivel]]--;
-    asigT[nivel]--;
+    libres++;
+    s[nivel]=-1;
     nivel--;
 }
 
-bool criterio(int nivel, Solucion& s, vector<int>& asigW){
+int cotaSuperior(int nivel, int ben, vector<int>& asigW) {
+    int cota = ben;
+    for (int j = nivel + 1; j < nt; j++) {
+        int maxB = 0;
+        for (int i = 0; i < nw; i++) {
+            if (asigW[i] < C[i] && B[i][j] > maxB)
+                maxB = B[i][j];
+        }
+        cota += maxB;
+    }
+    return cota;
+}
 
-    return nivel < nt- 1 && s[nivel] < nw -1 && asigW[s[nivel]] < C[s[nivel]] && B[s[nivel]][nivel]!=0;
+bool criterio(int &nivel, Solucion& s, vector<int>& asigW, int ba, int bm){
+
+    return nivel < nt - 1 && 
+        asigW[s[nivel]] <= C[s[nivel]] && 
+        B[s[nivel]][nivel]!=0 &&
+        !(cotaSuperior(nivel, ba, asigW) <= bm);
 
 }
 
-bool solucion(int nivel, Solucion& s, vector<int>& asigT){
+bool solucion(int &nivel, Solucion& s, vector<int>& asigW){ //necesario para comprobar que la solucion es finalmente válida
 
-    return nivel==nt - 1;
+    return nivel == nt - 1 && 
+        asigW[s[nivel]] <= C[s[nivel]] && 
+        B[s[nivel]][nivel]!=0;
 
 }
 
@@ -68,62 +92,69 @@ bool trabajadoresLibres(vector<int>& asig){
 
 }
 
-bool masHermanos(int nivel, Solucion& s, vector<int>& asigW){
+bool masHermanos(int &nivel, Solucion& s){
 
-    return s[nivel] < nw - 1 && trabajadoresLibres(asigW);
+    return s[nivel] < nw - 1 && libres!=0;
 
 }
 
 int main(){
 
-    cin>>nw>>nt;
+    cin>>T;
 
-    C = vector<int>(nw);
-    B = vector<vector<int>>(nw);
-    vector<int> asignados_trabajos=vector<int>(nt, 0);
-    vector<int> asignados_workers=vector<int>(nw, 0);
-    
-    for(int i=0; i<nw; i++){
-        for(int j=0; j<nt; j++){
-            cin>>B[i][j];
-        }
-    }
+    for(int k=0; k<T; k++){
 
-    for(int i=0; i<nw; i++){
-        cin>>C[i];
-    }
+        cin>>nw>>nt;
 
-    Solucion solucion_maxima=vector<int>(nt, -1);
-    int beneficio_maximo=0;
-    Solucion solucion_actual=vector<int>(nt, -1);
-    int beneficio_actual=0;
-
-    int nivel=0;
-
-    while(nivel!=-1){
-
-        generar(nivel, solucion_actual, beneficio_actual, asignados_trabajos, asignados_workers);
-
-        if(solucion(nivel, solucion_actual, asignados_trabajos)){
-            if(beneficio_actual>=beneficio_maximo){
-                beneficio_maximo=beneficio_actual;
-                solucion_maxima=solucion_actual;
+        C = vector<int>(nw);
+        B = vector<vector<int>>(nw, vector<int>(nt));
+        vector<int> asignados_workers=vector<int>(nw, 0);
+        
+        for(int i=0; i<nw; i++){
+            for(int j=0; j<nt; j++){
+                cin>>B[i][j];
             }
         }
 
-        if(criterio(nivel, solucion_actual, asignados_workers)){
-            nivel++;
+        for(int i=0; i<nw; i++){
+            cin>>C[i];
         }
-        else{
-            while(nivel>=0 && !masHermanos(nivel, solucion_actual, asignados_workers)){
-                retroceder(nivel, solucion_actual, beneficio_actual, asignados_workers, asignados_trabajos);
+
+        libres=0;
+        for (int i = 0; i < nw; i++) libres += C[i];
+
+        Solucion solucion_maxima=vector<int>(nt, -1);
+        int beneficio_maximo=0;
+        Solucion solucion_actual=vector<int>(nt, -1);
+        int beneficio_actual=0;
+
+        int nivel=0;
+
+        while(nivel!=-1){
+
+            generar(nivel, solucion_actual, beneficio_actual, asignados_workers);
+
+            if(solucion(nivel, solucion_actual, asignados_workers)){ //Podria ser que almacenara soluciones innecesaria
+
+                if(beneficio_actual>=beneficio_maximo){
+                    beneficio_maximo=beneficio_actual;
+                    solucion_maxima=solucion_actual;
+                }
             }
+
+            if(criterio(nivel, solucion_actual, asignados_workers, beneficio_actual, beneficio_maximo)){ 
+                nivel++;
+            }
+            else{
+                while(nivel>=0 && !masHermanos(nivel, solucion_actual)){
+                    retroceder(nivel, solucion_actual, beneficio_actual, asignados_workers);
+                }
+            }
+
         }
 
+        cout<<beneficio_maximo<<endl;
     }
-
-    cout<<beneficio_maximo<<endl;
-
 
     return 0;
 }
